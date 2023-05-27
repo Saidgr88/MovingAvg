35b1955bdb1bcb946ec10d9ffc1489793c73d528
Saidgr88 <118079058+Saidgr88@users.noreply.github.com>
2023-05-27 04:52:24 +0300

    Update movingAvg.h

--- MovingAverage/movingAvg.h
+++ MovingAverage/movingAvg.h
@@ -5,13 +5,39 @@
 #include <iostream>
 #include <cstdlib> // для функций rand() и srand()
 #include <ctime> // для функции time()
-#include <map>
 #include <cmath>
 
 
 double getRandomNumber(int min, int max);
 //int getRandomNumber(int min, int max);
-double movingAverage(double *values, int size);
-float movingAverage(float *values, int size);
+double average(double *values, int size);
+float average(float *values, int size);
+
+
+template<typename T>
+int movingAverage(T *data, size_t size, size_t window)
+{
+    if(size <= window) return 1;
+    
+    T windowValues[window + 1] {};
+    windowValues[0] = data[0];
+    
+    for (size_t a = 0; a < window + 1; a++)
+        windowValues[a] = data[a];
+
+    for (size_t i = 0; i < size - window + 1; i++)
+    {
+        int curr = i;
+        windowValues[window] = data[curr + window];
+        
+        data[i + window - 1] = average(windowValues,window);
+        
+        for(size_t b = 0; b < window; b++)
+            windowValues[b] = windowValues[b + 1];
+    }
+   
+    return 0;
+}
+
 
 #endif
