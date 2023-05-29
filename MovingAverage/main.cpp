898b8354e02dfcf29369300d7b567a88d69d9496
Saidgr88 <118079058+Saidgr88@users.noreply.github.com>
2023-05-29 21:02:49 +0300

    Update main.cpp

--- MovingAverage/main.cpp
+++ MovingAverage/main.cpp
@@ -1,15 +1,19 @@
 #include "movingAvg.h"
-#include <vector>
+
+
+
 
 int main()
 {
     srand(static_cast<unsigned int>(time(0)));
 
-    size_t size = 2;
+    size_t window = 2;
     size_t lenData = 1000000;
 
     std::vector<float> floatTest;
+    std::vector<float> floatTestOut;
     std::vector<double> doubleTest;
+    std::vector<double> doubleTestOut;
 
     for (size_t i = 0; i < lenData; i++)
     {
@@ -28,12 +32,15 @@ int main()
     
     unsigned int zeroTime;
     
-    while(size <= 128)
+    while(window <= 128)
     {
+        floatTestOut.clear();
+        doubleTestOut.clear();
+
         zeroTime = clock();
-        if(!movingAverage(&doubleTest[0],lenData,size))
+        if(!movingAverage(doubleTest,doubleTestOut,window))
         {
-            std::cout << "\nTime for window " << size << " for double: " << clock() - zeroTime << " mcseconds\n";
+            std::cout << "\nTime for window " << window << " for double: " << clock() - zeroTime << " mcseconds\n";
             
         }
         else
@@ -42,21 +49,19 @@ int main()
         }
 
         zeroTime = clock();
-        if(!movingAverage(&floatTest[0],lenData,size))
+        if(!movingAverage(floatTest,floatTestOut,window))
         {
-            std::cout << "\nTime for window " << size << " for float: " << clock() - zeroTime << " mcseconds\n";
+            std::cout << "\nTime for window " << window << " for float: " << clock() - zeroTime << " mcseconds\n";
             
         }
         else
         {
             std::cout << "Window size error'\n";
         }
-
-        size *= 2;
+        
+        window *= 2;
     }
-    
-    doubleTest.erase(doubleTest.begin(),doubleTest.begin()+127);
-    floatTest.erase(floatTest.begin(),floatTest.begin()+127);
+
     
     return 0;
 }
